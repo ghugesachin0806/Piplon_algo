d0505c83ac98eb6b5827cf3f1dd05a123b733788
Sachin Ghuge <ghugesachin4545@gmail.com>
2025-02-13 20:58:34 +0530

    changes as per first draft

--- algo.cpp
+++ algo.cpp
@@ -1,14 +1,186 @@
 #include "libraries.h"
 using namespace std;
 
+// golbal variables
+int batsmen;
+int bowlers;
+int overs;
+int overs_per_batsman; //
+int overs_per_bowler;  //
+int fielders_from_batting_side;
+int fielders_from_bowlers_side;
+int battingside_rotation_per_overs;
+int bowlingside_rotation_per_overs;
 
+vector<int> possible_overs;
 
-void intial_fun()
+// functions
+void init()
 {
+    batsmen = 0;
+    bowlers = 0;
+    overs = 0;
+    overs_per_batsman = 0;
+    overs_per_bowler = 0;
+    fielders_from_batting_side = 0;
+    fielders_from_bowlers_side = 0;
+    battingside_rotation_per_overs = 0;
+    bowlingside_rotation_per_overs = 0;
+}
+
+int selectBatsmen()
+{
+    cout << "select batsmen :";
+    cin >> batsmen;
+
+    if (batsmen < MIN_BATSMEN && batsmen > MAX_BATSMEN)
+        return 1;
+    else
+        return 0;
+}
+
+int selectBowlers()
+{
+    cout << "select bowlers :";
+    cin >> bowlers;
+
+    if (lcm(batsmen, bowlers) > MAX_OVERS_ALLOWED)
+        return 1;
+    else
+        return 0;
+}
+
+void oversSelect()
+{
+    cout << "Over choices : ";
+
+    int temp = lcm(batsmen, bowlers);
+
+    int i = 1;
+    while (temp <= MAX_OVERS_ALLOWED)
+    {
+        i++;
+        cout << temp << " ";
+        possible_overs.push_back(temp);
+        temp = temp * i;
+    }
+    cout << endl;
+
+    cout << "Enter total overs you want: ";
+
+    int inputTemp = 0;
+    cin >> inputTemp;
+
+    bool flag = true;
+
+    for (int i = 0; i < possible_overs.size(); i++)
+    {
+        if (inputTemp == possible_overs[i])
+        {
+            flag = false;
+            overs = possible_overs[i];
+            overs_per_batsman = possible_overs[i] / batsmen;
+            overs_per_bowler = possible_overs[i] / bowlers;
+            cout << "Overs selected: " << overs << endl;
+            cout << "overs per batsman: " << possible_overs[i] / batsmen << endl;
+            cout << "Overs per bowler: " << possible_overs[i] / bowlers << endl;
+            break;
+        }
+    }
+
+    if (flag)
+        cout << "Invalid Input !!!" << endl;
+}
+
+int selectCombinationFielders()
+{
+    int tempbatsmen;
+    int tempbowlers;
+    cout << "select batsmen for fielding from range : " << 0 << " - " << batsmen - 1 << " : ";
+    cin >> tempbatsmen;
+    cout << "select bowlers for fielding from tange : " << 0 << " - " << bowlers - 1 << " : ";
+    cin >> tempbowlers;
+
+    if ((tempbatsmen < 0 || tempbatsmen >= batsmen) || (tempbowlers < 0 || tempbowlers >= bowlers))
+        return 0;
+    else
+    {
+        fielders_from_batting_side = tempbatsmen;
+        fielders_from_bowlers_side = tempbowlers;
+
+        return 1;
+    }
+}
+
+int selectRotation()
+{
+    vector<int> bowlerRotationVect;
+    cout << "Rotation for bowlers: ";
+
+    for (int i = 0; i <= sqrt(overs_per_bowler); i++)
+    {
+        if (overs_per_bowler % i == 0)
+        {
+            bowlerRotationVect.push_back(i);
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    cout<<"Select battingside rotation choice from above: ";
+    cin>>bowlingside_rotation_per_overs;
+
+
+    vector<int> batsmanRotationVect;
+    cout << "Rotation for bowlers: ";
+
+    for (int i = 0; i <= sqrt(overs_per_batsman); i++)
+    {
+        if (overs_per_batsman % i == 0)
+        {
+            batsmanRotationVect.push_back(i);
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    cout<<"Select bowlerside rotation choice from above: ";
+    cin>>bowlingside_rotation_per_overs;
 
 }
 
 int main()
 {
+    init();
 
+    // select batsmen
+    if (selectBatsmen())
+    {
+        cout << "Invalid Input" << endl;
+        return 1;
+    }
+
+    // select bowlers
+    if (selectBowlers())
+    {
+        cout << "Invalid Input" << endl;
+        return 1;
+    }
+
+    // select total overs
+    oversSelect();
+
+    // select combination of fielders
+    if (selectCombinationFielders())
+    {
+        cout << "Invalid Input" << endl;
+        return 1;
+    }
+
+    // Rotation choices allowed for batsmen and bowlers
+    if (selectRotation())
+    {
+        cout << "Invalid Input" << endl;
+        return 1;
+    }
 }
--- libraries.h
+++ libraries.h
@@ -42,91 +42,15 @@
 #include <climits>
 #include <cassert>
 
+// Annexure - 0
+#define MAX_OVERS_ALLOWED (50)
 
-// Annexure - 1
-#define NO_BATTERS (18)
-// Annexure - 2
-#define NO_BOWLERS (18)
-// Annexure - 3
-#define COMPOSITION_VARIANT (20)
-// Annexure - 4
-#define ROTATION_OF_FIELDERS_VARIANT (6)
-// Annexure - 5
-#define OVERS_PER_PLAYER (6)
+// Annexure - I
+#define MIN_BATSMEN (3)
+#define MAX_BATSMEN (20)
 
-// Annexure - 3
-typedef struct Composition_fielsers_on_grd
-{
-    uint32_t variant;
-    uint32_t f_batting;
-    uint32_t f_bowling;
-    uint32_t wicketkeeper;
-    bool wkt_bat_org;
-    uint32_t total;
+// Annexure - II
+#define MIN_BOWLERS (3)
+#define MAX_BOWLERS (20)
 
-} compstn_fldrs_on_grd;
-
-// Annexure - 4
-typedef struct Rotation_of_fielder
-{
-    uint32_t variant;
-    uint32_t batters_after_over;
-    uint32_t bowlers_after_over;
-} rotation_of_fielder;
-
-// Annexure - 5
-typedef struct Over_per_player
-{
-    uint32_t variant;
-    uint32_t bowler_bowling;
-    uint32_t batsmen_rest;
-    uint32_t batsmen_batting;
-} over_per_player;
-
-// Annexure - 1
-uint8_t no_batters[NO_BATTERS] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-
-// Annexure - 2
-uint8_t no_bowlers[NO_BATTERS] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-
-// Annexure - 3
-compstn_fldrs_on_grd compositions[COMPOSITION_VARIANT] =
-    {
-        {1, 2, 3, 0, 0, 5},
-        {2, 3, 3, 0, 0, 6},
-        {3, 3, 4, 0, 0, 7},
-        {4, 4, 4, 0, 0, 8},
-        {5, 4, 5, 0, 0, 9},
-        {6, 5, 5, 0, 0, 10},
-        {7, 5, 6, 0, 0, 11},
-        {8, 6, 5, 0, 0, 11},
-        {9, 7, 4, 0, 0, 11},
-        {10, 8, 3, 0, 0, 11},
-        {11, 9, 2, 0, 0, 11},
-        {12, 10, 1, 0, 0, 11},
-        {13, 6, 4, 1, 0, 11},
-        {14, 7, 3, 1, 0, 11},
-        {15, 8, 2, 1, 0, 11},
-        {16, 9, 1, 1, 0, 11},
-        {17, 8, 4, 0, 0, 12},
-        {18, 9, 3, 0, 0, 12},
-        {19, 10, 2, 0, 0, 12},
-        {20, 11, 1, 0, 0, 12}};
-
-// Annexure - 4
-rotation_of_fielder rotaions[ROTATION_OF_FIELDERS_VARIANT] = {
-    {1, 1, 1},
-    {2, 2, 1},
-    {3, 2, 2},
-    {4, 3, 1},
-    {5, 3, 3},
-    {6, 4, 4}};
-
-// Annexure - 5
-over_per_player over_per_players[OVERS_PER_PLAYER] = {
-    {1, 1, 1, 1},
-    {2, 2, 2, 2},
-    {3, 3, 3, 3},
-    {4, 4, 4, 4},
-    {5, 5, 5, 5},
-    {6, 6, 6, 6}};
+// Annexure - III
